a19203b4b4ab909767b21cfbe5ed78c3edcb4e40
Haoxiang310 <gengh@usc.edu>
2023-07-25 21:35:19 -0700

    07/25/2023 commit

--- Leetcode101-200/Leetcode144.java
+++ Leetcode101-200/Leetcode144.java
@@ -14,3 +14,18 @@ class Solution {
         traverse(root.right);
     }
 }
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        if(root!=null) stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode curr = stack.pop();
+            res.add(curr.val);
+            if(curr.right!=null) stack.push(curr.right);
+            if(curr.left!=null) stack.push(curr.left);
+        }
+        return res;
+    }
+}
+
