ed23261a03d52f05ce659fe39cc60631b57d1dfc
Haoxiang310 <gengh@usc.edu>
2023-07-29 22:00:31 -0600

    07/29/2023 commit

--- Leetcode201-300/Leetcode226.java
+++ Leetcode201-300/Leetcode226.java
@@ -12,3 +12,19 @@ class Solution {
         return root;
     }
 }
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    public void traverse(TreeNode root){
+        if(root==null) return;
+        traverse(root.left);
+        traverse(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+}
