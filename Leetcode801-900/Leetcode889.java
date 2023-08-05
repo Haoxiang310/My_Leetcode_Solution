f4bba9de78a2e40efcf0e79aedea212cd6320f59
Haoxiang310 <gengh@usc.edu>
2023-08-04 23:18:30 -0400

    08/04/2023 commit

--- Leetcode801-900/Leetcode889.java
+++ Leetcode801-900/Leetcode889.java
@@ -20,3 +20,32 @@ class Solution {
         return node;
     }
 }
+
+O(N) solution
+class Solution {
+    Map<Integer,Integer> map = new HashMap<>();
+    int[] preorder, postorder;
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        if(preorder.length == 0) return null;
+        this.preorder = preorder;
+        this.postorder = postorder;
+        for(int i=0;i<postorder.length;i++){
+            map.put(postorder[i],i);
+        }
+        return buildHelper(0,preorder.length-1,0,postorder.length-1);
+    }
+
+    public TreeNode buildHelper(int preStart, int preEnd, int postStart, int postEnd){
+        if(preStart > preEnd || postStart > postEnd) return null;
+ 
+        if(preStart == preEnd){
+            return new TreeNode(preorder[preStart]);
+        }
+               TreeNode root = new TreeNode(preorder[preStart]);
+        int index = map.get(preorder[preStart+1]);
+        int leftTreeSize = index - postStart;
+        root.left = buildHelper(preStart+1,preStart+1+leftTreeSize,postStart,postStart+leftTreeSize);
+        root.right = buildHelper(preStart+2+leftTreeSize,preEnd,postStart+leftTreeSize+1,postEnd-1);
+        return root;
+    }
+}
